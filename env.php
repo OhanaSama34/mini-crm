2178cd004c4aa39f36f28567834cc7287fcae206
Noufal Radhitya <94808421+OhanaSama34@users.noreply.github.com>
2024-12-03 23:51:28 +0700

    Update env.php

--- env.php
+++ env.php
@@ -4,10 +4,10 @@
 
 
 //enviroment variabel
-$db_host = "localhost";
-$db_user = "root";
+$db_host = "";
+$db_user = "";
 $db_pass = "";
-$db_name = "db_ekatalog_crm";    
+$db_name = "";    
 
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
